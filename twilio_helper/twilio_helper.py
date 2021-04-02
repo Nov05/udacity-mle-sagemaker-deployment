1f5162c375c4555d18ad78097b43ee9e2c074e25
EC2 Default User <ec2-user@ip-172-16-170-216.ec2.internal>
2021-04-02 04:05:55 +0000

    2021-04-01 AWS SageMaker Lifecircle Configurations

--- twilio_helper/twilio_helper.py
+++ twilio_helper/twilio_helper.py
@@ -1,19 +1,19 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
-# import os
+import os
 import json
 from twilio.rest import Client
 from pathlib import Path
 
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-# account_sid = os.environ['TWILIO_ACCOUNT_SID']
-# auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 filename = str(Path(__file__).parent.absolute()) + '/twilio.json'
 with open(filename) as f:
     twilio_config = json.load(f)
-
-client = Client(twilio_config['account_sid'], twilio_config['auth_token'])
+# client = Client(twilio_config['account_sid'], twilio_config['auth_token'])
 
 def send_text_message(body='Text message from Twilio',
                     #   media_url=['https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg']
